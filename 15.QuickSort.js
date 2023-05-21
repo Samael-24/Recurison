25655b9057cddc1b10119bb7e61e873f4b495d46
Samael-24 <parishk1024@gmail.com>
2023-05-21 21:06:56 +0530

    Formatting done

--- 15.QuickSort.js
+++ 15.QuickSort.js
@@ -23,9 +23,9 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
       quickSort(arr, pivot + 1, right)
     }
     return arr
-  }
+}
   
-  function partition(arr, left, right) {
+function partition(arr, left, right) {
     const pivot = arr[right]
     let i = left
     for (let j = left; j < right; j++) {
@@ -36,15 +36,15 @@ function quickSort(arr, left = 0, right = arr.length - 1) {
     }
     swap(arr, i, right)
     return i
-  }
+}
   
-  function swap(arr, i, j) {
+function swap(arr, i, j) {
     const temp = arr[i]
     arr[i] = arr[j]
     arr[j] = temp
-  }
+}
   
-  const arr = [8, 20, -2, 4, -6]
-  quickSort(arr)
-  console.log(arr) // [-6, -2, 4, 8, 20]
+const arr = [8, 20, -2, 4, -6]
+quickSort(arr)
+console.log(arr) // [-6, -2, 4, 8, 20]
   
